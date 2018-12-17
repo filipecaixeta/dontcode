3caaca7c80ee47fa671dfa404e5ac46b06bab46f
Filipe Caixeta <filipecaixeta@gmail.com>
2018-12-17 20:02:45 -0200

    change host4

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // BACKEND_SOCKETIO: 'http://35.190.159.199:8080'
-  BACKEND_SOCKETIO: 'http://localhost:8080'
+  BACKEND_SOCKETIO: 'http://35.190.159.199:8080'
+  // BACKEND_SOCKETIO: 'http://localhost:8080'
 }
